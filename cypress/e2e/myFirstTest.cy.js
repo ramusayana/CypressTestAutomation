321948dcc8d5d2403000c3856104f2d3d0232404
ramusayana <ramusayana@gmail.com>
2024-11-17 17:04:46 +0000

    my first test

---
+++ cypress/e2e/myFirstTest.cy.js
@@ -0,0 +1,19 @@
+describe("My First Test", function () {
+  
+  it("verify happy path", function () {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+
+    cy.title().should("eq", "OrangeHRM");
+  });
+
+  it("verify negative test", function () {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+
+    cy.title().should("eq", "OrangeHMRC");
+  });
+
+});
